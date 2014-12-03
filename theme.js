5e2ee42ef8bfe7ff31119f63837f8ec8d2bdfd1a
Miguel Pignatelli <emepyc@gmail.com>
2014-12-03 01:15:23 +0000

    wip

--- theme.js
+++ theme.js
@@ -1,8 +1,11 @@
+
 var tnt_theme = function () {
+    scale = false;
     var theme = function (t, div) {
 	t
 	    .layout (tnt.tree.layout.vertical()
-		     .width(650)
+		     .width(250)
+		     .scale(scale)
 		    )
 	    .label (tnt.tree.label.text()
 		    .height(15)
@@ -10,5 +13,21 @@ var tnt_theme = function () {
 	t(div);
     };
 
+    // scale method
+    theme.scale = function (v) {
+	if (!arguments.length) {
+	    return scale;
+	}
+	switch (v) {
+	case "false" :
+	    scale = false;
+	    break;
+	case "true" :
+	    scale = true;
+	    break;
+	}
+	return theme;
+    };
+    
     return theme;
 };
